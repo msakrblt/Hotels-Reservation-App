978dc7c9d3b641f57a5972432f60c03d8679795a
Musa_Karabulut <musa.krblt.23@gmail.com>
2021-11-20 13:36:19 +0300

    metin

--- src/main/java/com/musakarabulut/hotelreservation/HotelReservationApplication.java
+++ src/main/java/com/musakarabulut/hotelreservation/HotelReservationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelReservationApplication {
 	public static void main(String[] args) {
+		System.out.println("Metin");
 		SpringApplication.run(HotelReservationApplication.class, args);
 	}
 
